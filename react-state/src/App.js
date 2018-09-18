237b5790133fef8461807b42b7a20f43e39dd388
Maria Bazhlekova <mbazhlek@gmail.com>
2018-09-18 06:41:52 -0500

    Add Card component

--- react-state/src/App.js
+++ react-state/src/App.js
@@ -1,16 +1,32 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
+import { Card, Elevation } from "@blueprintjs/core";
+import styled from "styled-components";
+
 import Form from "./Form";
 import GlobalStyle from "./globalStyle";
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <div>
         <GlobalStyle />
-        <Form />
-      </Fragment>
+        <CenteredCard interactive={true} elevation={Elevation.THREE}>
+          <Form />
+        </CenteredCard>
+      </div>
     );
   }
 }
+
+const CenteredCard = styled(Card)`
+  position: absolute;
+  margin: auto;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 50%
+  height: 50%
+`;
 
 export default App;
