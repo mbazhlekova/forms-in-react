5a3d669580e415b85708824cc38b640079d1dc99
Maria Bazhlekova <mbazhlek@gmail.com>
2018-09-18 06:42:35 -0500

    Import blueprint css

--- react-state/src/index.js
+++ react-state/src/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "../node_modules/@blueprintjs/core/lib/css/blueprint.css";
+import "../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
