59988418e243b7f51534f6dfd96db4fa8f242d8f
Maria Bazhlekova <mbazhlek@gmail.com>
2018-09-18 06:42:23 -0500

    Add background color

--- react-state/src/globalStyle.js
+++ react-state/src/globalStyle.js
@@ -4,7 +4,9 @@ const GlobalStyle = createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
-    font-family: sans-serif; 
+    font-family: sans-serif;
+    background: rgba(0, 0, 0, 0)
+    linear-gradient(rgb(230, 100, 101), rgb(145, 152, 229)) fixed;
   }
 `;
 
